0df6918a29250a8c7d566bd94e0f5eda10e4e7ee
Matheus de Brito Milani <matheusdebritomilani@gmail.com>
2021-08-27 09:33:53 -0300

    Add files via upload

---
+++ projetoG5RedeSocial/src/test/java/com/projetoG5RedeSocial/ProjetoG5RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoG5RedeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoG5RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
